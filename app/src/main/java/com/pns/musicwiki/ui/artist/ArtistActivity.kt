d3715eb592c7c3a8fd3492c37d0e25e78915efa6
Vipin dev <officialvipindev@gmail.com>
2023-04-06 18:32:12 +0530

    create artist activity

---
+++ app/src/main/java/com/pns/musicwiki/ui/artist/ArtistActivity.kt
@@ -0,0 +1,12 @@
+package com.pns.musicwiki.ui.artist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.pns.musicwiki.R
+
+class ArtistActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_artist)
+    }
+}
