cf6e9d6e6751eb08d50d9fdb4096536c5738b084
Vipin dev <officialvipindev@gmail.com>
2023-04-06 19:21:01 +0530

    create adapter for tracks

---
+++ app/src/main/java/com/pns/musicwiki/ui/genredetails/adapter/TracksAdapter.kt
@@ -0,0 +1,40 @@
+package com.pns.musicwiki.ui.genredetails.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.pns.musicwiki.data.genredetails.tracks.Track
+import com.pns.musicwiki.databinding.GenreDetailItemLayoutBinding
+
+class TracksAdapter(private val trackList: MutableList<Track>) :
+    RecyclerView.Adapter<TracksAdapter.GenreTracksViewHolder>() {
+
+    inner class GenreTracksViewHolder(val binding: GenreDetailItemLayoutBinding) :
+        RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GenreTracksViewHolder {
+        return GenreTracksViewHolder(
+            GenreDetailItemLayoutBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: GenreTracksViewHolder, position: Int) {
+
+        val track = trackList[position]
+
+        holder.binding.apply {
+            title.text = track.name
+            artist.text = track.name
+            Glide.with(image).load(track.image[0].text).into(image)
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return trackList.size
+    }
+}
