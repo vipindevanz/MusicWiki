e2c5ab84c1cc48dfa7d28930a1d578b864418a54
Vipin dev <officialvipindev@gmail.com>
2023-04-06 18:51:04 +0530

    create repository for track

--- app/src/main/java/com/pns/musicwiki/repository/Repository.kt
+++ app/src/main/java/com/pns/musicwiki/repository/Repository.kt
@@ -4,6 +4,7 @@ import com.pns.musicwiki.data.genre.GenreResponse
 import com.pns.musicwiki.data.genredetails.GenreDetailsResponse
 import com.pns.musicwiki.data.genredetails.album.AlbumResponse
 import com.pns.musicwiki.data.genredetails.artist.ArtistResponse
+import com.pns.musicwiki.data.genredetails.tracks.TrackResponse
 import com.pns.musicwiki.data.network.ApiClient
 import retrofit2.Response
 
@@ -21,7 +22,11 @@ class Repository {
         return ApiClient.getWikiApi().getAlbums(genre)
     }
 
-    suspend fun getTagArtist(genre:String):Response<ArtistResponse>{
+    suspend fun getGenreArtist(genre:String):Response<ArtistResponse>{
         return ApiClient.getWikiApi().getArtists(genre)
+    }
+
+    suspend fun getGenreATracks(genre: String):Response<TrackResponse>{
+        return ApiClient.getWikiApi().getTracks(genre)
     }
 }
