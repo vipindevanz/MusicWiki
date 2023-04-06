96316c795df0be99b2185b081a5c6a9f2912c6ca
Vipin dev <officialvipindev@gmail.com>
2023-04-06 17:10:25 +0530

    create album activity

---
+++ app/src/main/java/com/pns/musicwiki/ui/album/AlbumActivity.kt
@@ -0,0 +1,12 @@
+package com.pns.musicwiki.ui.album
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.pns.musicwiki.R
+
+class AlbumActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_album)
+    }
+}
